1501c41300f8c7c67f310fe940233b227530851b
Josh Bivens <thejoshbivens@gmail.com>
2024-05-19 20:31:45 -0600

    Update Wordle.py
    Milestone 2

--- Wordle.py
+++ Wordle.py
@@ -13,14 +13,25 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        # Build word from board
+        i = 0
+        word = ""
+        while i < N_COLS:
+            word += gw.get_square_letter(0, i)
+            i += 1
+
+        # MILESTONE 2
+        if word.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("Word Found!")
+        else:
+            gw.show_message("Word not in list!")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
     # MILESTONE 1
-    for i, letter in enumerate(random.choice(FIVE_LETTER_WORDS)):
-        gw.set_square_letter(0, i, letter)
+    # for i, letter in enumerate(random.choice(FIVE_LETTER_WORDS)):
+    #     gw.set_square_letter(0, i, letter)
 
 # Startup code
 
